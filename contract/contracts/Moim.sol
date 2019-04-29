a0dfb8ce2a996e2afe7ce83ad95b6fb6c86014e1
배지원(conan) <conan.bae@groundx.xyz>
2019-04-29 17:09:38 +0900

    Moim Contract - Create New Loan - Update Loan - get Loan Info
    @TODO
    - It should be possible to obtain factor values for several people.

---
+++ contract/contracts/Moim.sol
@@ -0,0 +1,122 @@
+pragma solidity 0.5.0;
+
+/**
+* @title SafeMath
+* @dev Unsigned math operations with safety checks that revert on error
+*/
+library SafeMath {
+   /**
+    * @dev Multiplies two unsigned integers, reverts on overflow.
+    */
+   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+       // Gas optimization: this is cheaper than requiring ‘a’ not being zero, but the
+       // benefit is lost if ‘b’ is also tested.
+       // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+       if (a == 0) {
+           return 0;
+       }
+
+       uint256 c = a * b;
+       require(c / a == b);
+
+       return c;
+   }
+
+   /**
+    * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
+    */
+   function div(uint256 a, uint256 b) internal pure returns (uint256) {
+       // Solidity only automatically asserts when dividing by 0
+       require(b > 0);
+       uint256 c = a / b;
+       // assert(a == b * c + a % b); // There is no case in which this doesn’t hold
+
+       return c;
+   }
+
+   /**
+    * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+    */
+   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+       require(b <= a);
+       uint256 c = a - b;
+
+       return c;
+   }
+
+   /**
+    * @dev Adds two unsigned integers, reverts on overflow.
+    */
+   function add(uint256 a, uint256 b) internal pure returns (uint256) {
+       uint256 c = a + b;
+       require(c >= a);
+
+       return c;
+   }
+
+   /**
+    * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
+    * reverts when dividing by zero.
+    */
+   function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+       require(b != 0);
+       return a % b;
+   }
+}
+
+contract LoanContract {
+   using SafeMath for uint256;
+
+   event NewLoanCreation(address bank, address target, uint256 value);
+
+   struct Loan {
+       address borrow; // naming change
+       address lender; // naming change
+       uint256 totalAmount;
+       uint256 totalPayBackedAmount;
+       uint256 loanStartTime;
+       uint256[] payBackedAmount;
+       uint256[] payBackedTime;
+   }
+
+   mapping(bytes32 => Loan) loanMap;
+
+   function getId(address bankAddr, address targetAddr) private pure returns(bytes32) {
+       return keccak256(abi.encodePacked(bankAddr,targetAddr));
+   }
+
+   function createNewLoan(address _borrowAddr, address _lenderAddr, uint256 _totalAmount) external {
+       bytes32 id = getId(_borrowAddr, _lenderAddr);
+    //   require(loanMap[id].loanStartTime == 0, “Already created loan.“);
+
+       loanMap[id] = Loan({
+               borrow : _borrowAddr,
+               lender : _lenderAddr,
+               totalAmount : _totalAmount,
+               totalPayBackedAmount : 0,
+               loanStartTime : now,
+               payBackedAmount : new uint256[](0),
+               payBackedTime : new uint256[](0)
+           });
+       emit NewLoanCreation( _borrowAddr, _lenderAddr, _totalAmount);
+   }
+
+   function payBackLoan(address borrowedAddr, address bankAddr, uint256 payBackVal) external {
+       bytes32 id = getId(bankAddr,borrowedAddr);
+    //   require(id != 0, “Not existed id.“);
+
+    //   require((loanMap[id].totalAmount - loanMap[id].totalPayBackedAmount) >= payBackVal, “Pay back value is larger than borrow.“);
+       loanMap[id].totalPayBackedAmount = loanMap[id].totalPayBackedAmount.add(payBackVal);
+       loanMap[id].payBackedAmount.push(payBackVal);
+       loanMap[id].payBackedTime.push(now);
+   }
+   
+   function getLoanInfo(address borrowedAddr, address bankAddr) public view returns(uint256, uint256, uint256) {
+        bytes32 _id = getId(bankAddr, borrowedAddr);
+        return(
+            loanMap[_id].totalAmount,
+            loanMap[_id].totalPayBackedAmount,
+            loanMap[_id].payBackedAmount.length
+        );
+    }
+}
