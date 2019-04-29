7c2144804940b3820031f6fb6049224a3565fedb
Keon Kim <keon@uber.com>
2019-04-28 20:28:41 -0700

    boilerplate

---
+++ moim-backend/config/passport.js
@@ -0,0 +1,110 @@
+const passport = require('passport');
+const { Strategy: FacebookStrategy } = require('passport-facebook');
+const _ = require('lodash');
+
+const User = require('../models/User');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+/**
+ * OAuth Strategy Overview
+ *
+ * - User is already logged in.
+ *   - Check if there is an existing account with a provider id.
+ *     - If there is, return an error message. (Account merging not supported)
+ *     - Else link new OAuth account with currently logged-in user.
+ * - User is not logged in.
+ *   - Check if it's a returning user.
+ *     - If returning user, sign in and we are done.
+ *     - Else check if there is an existing account with user's email.
+ *       - If there is, return an error message.
+ *       - Else create a new account.
+ */
+
+/**
+ * Sign in with Facebook.
+ */
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_ID,
+  clientSecret: process.env.FACEBOOK_SECRET,
+  callbackURL: `${process.env.BASE_URL}/auth/facebook/callback`,
+  profileFields: ['name', 'email', 'link', 'locale', 'timezone', 'gender'],
+  passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, done) => {
+  if (req.user) {
+    User.findOne({ facebook: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        done(err);
+      } else {
+        User.findById(req.user.id, (err, user) => {
+          if (err) { return done(err); }
+          user.facebook = profile.id;
+          user.tokens.push({ kind: 'facebook', accessToken });
+          user.profile.name = user.profile.name || `${profile.name.givenName} ${profile.name.familyName}`;
+          user.profile.gender = user.profile.gender || profile._json.gender;
+          user.profile.picture = user.profile.picture || `https://graph.facebook.com/${profile.id}/picture?type=large`;
+          user.save((err) => {
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ facebook: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      User.findOne({ email: profile._json.email }, (err, existingEmailUser) => {
+        if (err) { return done(err); }
+        if (existingEmailUser) {
+          done(err);
+        } else {
+          const user = new User();
+          user.email = profile._json.email;
+          user.facebook = profile.id;
+          user.tokens.push({ kind: 'facebook', accessToken });
+          user.profile.name = `${profile.name.givenName} ${profile.name.familyName}`;
+          user.profile.gender = profile._json.gender;
+          user.profile.picture = `https://graph.facebook.com/${profile.id}/picture?type=large`;
+          user.profile.location = (profile._json.location) ? profile._json.location.name : '';
+          user.save((err) => {
+            done(err, user);
+          });
+        }
+      });
+    });
+  }
+}));
+
+/**
+ * Login Required middleware.
+ */
+exports.isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+/**
+ * Authorization Required middleware.
+ */
+exports.isAuthorized = (req, res, next) => {
+  const provider = req.path.split('/').slice(-1)[0];
+  const token = req.user.tokens.find(token => token.kind === provider);
+  if (token) {
+    next();
+  } else {
+    res.redirect(`/auth/${provider}`);
+  }
+};
---
+++ moim-backend/controllers/api.js
@@ -0,0 +1,88 @@
+const graph = require('fbgraph');
+const paypal = require('paypal-rest-sdk');
+
+/**
+ * GET /api/facebook
+ * Facebook API example.
+ */
+exports.getFacebook = (req, res, next) => {
+  const token = req.user.tokens.find(token => token.kind === 'facebook');
+  graph.setAccessToken(token.accessToken);
+  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err, profile) => {
+    if (err) { return next(err); }
+    res.render('api/facebook', {
+      title: 'Facebook API',
+      profile
+    });
+  });
+};
+
+/**
+ * GET /api/paypal
+ * PayPal SDK example.
+ */
+exports.getPayPal = (req, res, next) => {
+  paypal.configure({
+    mode: 'sandbox',
+    client_id: process.env.PAYPAL_ID,
+    client_secret: process.env.PAYPAL_SECRET
+  });
+
+  const paymentDetails = {
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal'
+    },
+    redirect_urls: {
+      return_url: process.env.PAYPAL_RETURN_URL,
+      cancel_url: process.env.PAYPAL_CANCEL_URL
+    },
+    transactions: [{
+      description: 'Hackathon Starter',
+      amount: {
+        currency: 'USD',
+        total: '1.99'
+      }
+    }]
+  };
+
+  paypal.payment.create(paymentDetails, (err, payment) => {
+    if (err) { return next(err); }
+    const { links, id } = payment;
+    req.session.paymentId = id;
+    for (let i = 0; i < links.length; i++) {
+      if (links[i].rel === 'approval_url') {
+        res.render('api/paypal', {
+          approvalUrl: links[i].href
+        });
+      }
+    }
+  });
+};
+
+/**
+ * GET /api/paypal/success
+ * PayPal SDK example.
+ */
+exports.getPayPalSuccess = (req, res) => {
+  const { paymentId } = req.session;
+  const paymentDetails = { payer_id: req.query.PayerID };
+  paypal.payment.execute(paymentId, paymentDetails, (err) => {
+    res.render('api/paypal', {
+      result: true,
+      success: !err
+    });
+  });
+};
+
+/**
+ * GET /api/paypal/cancel
+ * PayPal SDK example.
+ */
+exports.getPayPalCancel = (req, res) => {
+  req.session.paymentId = null;
+  res.render('api/paypal', {
+    result: true,
+    canceled: true
+  });
+};
