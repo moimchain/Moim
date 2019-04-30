176b635463c5a6126363ef279cf853be5a3289b5
배지원(conan) <conan.bae@groundx.xyz>
2019-04-30 18:17:34 +0900

    add contract

--- moim-backend/controllers/home.js
+++ moim-backend/controllers/home.js
@@ -1,14 +1,17 @@
+const contract = require('../utils/getContract');
+
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
-  let token = "empty"
-  if (req.user) {
-    token = req.user.tokens.find(token => token.kind === 'facebook');
-  }
-  res.json({
-    hello: 'world',
-    auth: token
-  });
-};
+    let token = "empty"
+    console.log(contract);
+    if (req.user) {
+        token = req.user.tokens.find(token => token.kind === 'facebook');
+    }
+    res.json({
+        hello: 'world',
+        auth: token
+    });
+}
